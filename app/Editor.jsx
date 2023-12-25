48d066844d030a11e9aaca41384b29a418157c88
junhojava <junho9341@gmail.com>
2023-12-25 23:59:19 +0900

    add load image from server

--- app/Editor.jsx
+++ app/Editor.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { AffineSchemas } from '@blocksuite/blocks/models';
+import { AffineSchemas } from "@blocksuite/blocks/models";
 import { createEmptyPage, DocEditor } from "@blocksuite/presets";
 import "@blocksuite/presets/themes/affine.css";
 import { Job, Schema, Workspace } from "@blocksuite/store";
@@ -16,7 +16,25 @@ export default function Editor({ content }) {
     const page = editor.page;
     const workspace = page.workspace;
 
-    const job = new Job({workspace});
+    const job = new Job({ workspace });
+
+    const keys = await workspace.blob.list();
+
+    if(keys.length > 0) {
+      const form = new FormData();
+  
+      for(const key of keys) {
+        const value = await workspace.blob.get(key);
+        const file = new File([value], key, {type: value.type});
+        
+        form.append("files", file);
+      }
+  
+      await fetch("http://localhost:8080/make/file", {
+        method: "POST",
+        body: form
+      });
+    }
 
     await create(await job.pageToSnapshot(page));
   };
@@ -25,24 +43,33 @@ export default function Editor({ content }) {
     const getEditor = async () => {
       const schema = new Schema().register(AffineSchemas);
       const workspace = new Workspace({ schema });
-  
-      const job = new Job({workspace});
-
+      
       const editor = new DocEditor();
+      
+      if (Object.keys(content).length > 0) {
+        const assets = await (await fetch("http://localhost:8080/make/file")).json();
+        const job = new Job({ workspace });
 
-      if(Object.keys(content).length > 0) {
+        for(const key of assets) {
+          const blob = await (await fetch(`http://localhost:8080/make/file/${key}`)).blob();
+          const value = new File([blob], key, { type: blob.type });
+  
+          job.assets.set(key, value);
+        }
+
         const page = await job.snapshotToPage(content);
         editor.page = page;
+
       } else {
         const page = createEmptyPage().init();
         editor.page = page;
       }
-  
-      if(editorRef.current.children.length === 0) {
+
+      if (editorRef.current.children.length === 0) {
         editorRef.current.appendChild(editor);
         editorRef.current.instance = editor;
       }
-    }
+    };
 
     getEditor();
   }, []);
@@ -50,8 +77,21 @@ export default function Editor({ content }) {
   return (
     <>
       <button onClick={handleSaveButton}>save document</button>
-      <button onClick={async () => {await create({}); location.reload();}}>remove document data</button>
-      <button onClick={() => {location.reload()}}>refresh page</button>
+      <button
+        onClick={async () => {
+          await create({});
+          location.reload();
+        }}
+      >
+        remove document data
+      </button>
+      <button
+        onClick={() => {
+          location.reload();
+        }}
+      >
+        refresh page
+      </button>
       <div ref={editorRef} />
     </>
   );
