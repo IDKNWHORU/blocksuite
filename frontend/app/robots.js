8d6b80b66fe72f061b47c4d86f0127a4fab5816c
IDKNWHORU <junho9341@gmail.com>
2024-02-17 22:09:49 +0900

    구글 봇 robots.txt 목록에 추가

--- frontend/app/robots.js
+++ frontend/app/robots.js
@@ -4,9 +4,20 @@ export default function robots() {
     process.env.NAVER_SEARCH_ADVISOR !== "" &&
     process.env.NAVER_SEARCH_ADVISOR != null;
 
+  const isGoogleSearchAllow =
+    process.env.GOOGLE_SEARCH_CONSOLE !== "" &&
+    process.env.GOOGLE_SEARCH_CONSOLE != null;
+
   if (isNaverSearchAllow === true) {
     rules.push({
       userAgent: "Yeti",
+      allow: ["/"],
+    });
+  }
+
+  if (isGoogleSearchAllow === true) {
+    rules.push({
+      userAgent: "Googlebot",
       allow: ["/"],
     });
   }
